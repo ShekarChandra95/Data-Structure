4e0602a4ec5191a604c35119550ee3cadac44a85
Chandrashekara C M <83018791+ShekarChandra95@users.noreply.github.com>
2026-08-11 20:45:38 +0530

    Time: 3 ms (20.67%) | Memory: 12.5 MB (47.86%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,12 +10,26 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not list1 or not list2:
-            return list1 or list2
+        #recursion
+        # if not list1 or not list2:
+        #     return list1 or list2
         
-        if list1.val <= list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        # if list1.val <= list2.val:
+        #     list1.next = self.mergeTwoLists(list1.next, list2)
+        #     return list1
+        # else:
+        #     list2.next = self.mergeTwoLists(list1, list2.next)
+        #     return list2
+
+        dummy = temp = ListNode(0)
+        while list1 != None and list2 != None:
+
+            if list1.val <list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+        temp.next = list1 or list2
+        return dummy.next
