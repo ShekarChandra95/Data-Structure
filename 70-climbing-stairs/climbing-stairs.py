8d6c6082b3c632328a20ad11e1df4b0d6b37d9e2
Chandrashekara C M <83018791+ShekarChandra95@users.noreply.github.com>
2026-08-27 22:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.17%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n ==0 or n == 1:
+            return 1
+        # return self.climbStairs(n-1) + self.climbStairs(n-2) -- recursion - time complexity O(2^n)
+        prev, curr = 1, 1
+
+        for i in range(2, n+1):
+            temp = curr
+            curr = prev + temp
+            prev = temp
+        return curr
