35160af77bebd0b7c6027063a8cc386226d42b9b
Chandrashekara C M <83018791+ShekarChandra95@users.noreply.github.com>
2026-09-13 21:09:39 +0530

    Improve division logic with overflow handling
    Added overflow handling and adjusted division logic.

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -3,15 +3,20 @@ class Solution:
         max_int = 2**31 - 1
         min_int = -2**31
 
+        # Handle overflow case
         if dividend == min_int and divisor == -1:
             return max_int
-
+            
+        # determine the sign result
         negative = (dividend<0) != (divisor <0)
-
+        
+        # work with absolute values
         dividend, divisor = abs(dividend), abs(divisor)
         quotient = 0
         while dividend >= divisor:
             temp, multiple = divisor, 1
+
+            # double the divisor until it's bigger than dividend
             while dividend >=(temp<<1):
                 temp<<=1
                 multiple <<=1
